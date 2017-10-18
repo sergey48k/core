8ccacba978a5afb9a50269654987e07053ee47fa
Anthony ESTEBE <anthony.estebe@gmail.com>
2017-10-18 10:35:31 +0700

    update listeners list

--- src/blockchains/index.js
+++ src/blockchains/index.js
@@ -1,6 +1,5 @@
 module.exports = async () => [
   await require('./ethereum')('MAINNET'),
   await require('./ethereum')('KOVAN'),
-  await require('./bitcoin')(),
-  await require('./bitcoin')(true)
+  await require('./bitcoin')()
 ]
