d9e4ecd9fced995826162e7f1284335e980359f9
krhubert <krhubert@gmail.com>
2018-08-09 20:53:25 +0200

    Remove list status test

--- cmd/service/list_test.go
+++ cmd/service/list_test.go
@@ -8,17 +8,6 @@ import (
 	"github.com/stvp/assert"
 )
 
-func TestServiceStatusString(t *testing.T) {
-	s := service.Service{Name: "TestServiceStatusString"}
-	status := serviceStatus{
-		service: &s,
-		status:  service.RUNNING,
-	}
-	assert.Contains(t, "[Running]", status.String())
-	assert.Contains(t, s.Hash(), status.String())
-	assert.Contains(t, s.Name, status.String())
-}
-
 func TestSort(t *testing.T) {
 	status := []serviceStatus{
 		{status: service.PARTIAL, service: &service.Service{Name: "Partial"}},
